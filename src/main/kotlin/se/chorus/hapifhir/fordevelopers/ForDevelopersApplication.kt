08aa528a8e0d4605efc7b23a6eb2188fd89ba184
Pétur Þór Valdimarsson <petur@omnivore.se>
2022-07-04 17:19:29 +0200

    Initial commit

---
+++ src/main/kotlin/se/chorus/hapifhir/fordevelopers/ForDevelopersApplication.kt
@@ -0,0 +1,11 @@
+package se.chorus.hapifhir.fordevelopers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForDevelopersApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForDevelopersApplication>(*args)
+}
---
+++ src/test/kotlin/se/chorus/hapifhir/fordevelopers/ForDevelopersApplicationTests.kt
@@ -0,0 +1,13 @@
+package se.chorus.hapifhir.fordevelopers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForDevelopersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
